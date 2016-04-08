1fc6237d6186f8c71d347919c945681450e94902
Dario Woloszin <3h2so4@gmail.com>
2016-04-08 18:11:45 -0300

    Adequacao da torre

--- src/com/Monument/Dario/TorreArk.java
+++ src/com/Monument/Dario/TorreArk.java
@@ -18,76 +18,324 @@ public class TorreArk
   static class Renderer implements GLEventListener,KeyListener{
               
         private float rquad = 0.0f;
-        private float rtri = 0.0f;
-        float x = 0.8f;
-        
-
-         
+                 
        public void display(GLAutoDrawable drawable) {
             GL gl = drawable.getGL();
 
         // Clear the drawing area
         gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
         // Reset the current matrix to the "identity"
+        //Torre
         gl.glLoadIdentity();
+        gl.glPushMatrix();
+        gl.glTranslatef(1.5f, 0.0f, -6.0f);
+        //gl.glRotatef(rquad, 0f, 1.0f, 0f);
+        gl.glBegin(GL.GL_QUADS);
+        // Torre Base
+            gl.glColor3f(0.83f,0.83f,0.83f);
+            //front
+            gl.glVertex3f( -0.5f, -0.5f, 0.5f);
+            gl.glVertex3f( 0.5f, -0.5f, 0.5f);
+            gl.glVertex3f( 0.38f, 0.5f, 0.38f);
+            gl.glVertex3f( -0.38f, 0.5f, 0.38f);
+            
+            //back
+            gl.glVertex3f( -0.5f, -0.5f, -0.5f);
+            gl.glVertex3f( 0.5f, -0.5f, -0.5f);
+            gl.glVertex3f( 0.38f, 0.5f, -0.38f);
+            gl.glVertex3f( -0.38f, 0.5f, -0.38f);
 
-        // Move the "drawing cursor" around
-        gl.glTranslatef(-1.5f, 0.0f, -6.0f);
-      
-      
-      gl.glLoadIdentity();
-      gl.glPushMatrix();
-      gl.glTranslatef(1.5f, 0.0f, -6.0f);
-      gl.glRotatef(rquad, 0f, 1.0f, 0f);
-      gl.glBegin(GL.GL_QUADS);           	// Draw A Quad
-      //Torre
-        
-        gl.glColor3f(0.47f,0.53f,0.42f);			// Set The Color To Cinza
-        gl.glVertex3f( 1.0f*x, 1.0f*x,-1.0f*x);			// Top Right Of The Quad (Top)
-        gl.glVertex3f(-1.0f*x, 1.0f*x,-1.0f*x);			// Top Left Of The Quad (Top)
-        gl.glVertex3f(-1.0f*x, 1.0f*x, 1.0f*x);			// Bottom Left Of The Quad (Top)
-        gl.glVertex3f( 1.0f*x, 1.0f*x, 1.0f*x);			// Bottom Right Of The Quad (Top)
-
-        gl.glVertex3f( 1.0f,-1.0f, 1.0f);			// Top Right Of The Quad (Bottom)
-        gl.glVertex3f(-1.0f,-1.0f, 1.0f);			// Top Left Of The Quad (Bottom)
-        gl.glVertex3f(-1.0f,-1.0f,-1.0f);			// Bottom Left Of The Quad (Bottom)
-        gl.glVertex3f( 1.0f,-1.0f,-1.0f);			// Bottom Right Of The Quad (Bottom)
-
-        gl.glVertex3f( 1.0f*x, 1.0f*x, 1.0f*x);			// Top Right Of The Quad (Front)
-        gl.glVertex3f(-1.0f*x, 1.0f*x, 1.0f*x);			// Top Left Of The Quad (Front)
-        gl.glVertex3f(-1.0f,-1.0f, 1.0f);			// Bottom Left Of The Quad (Front)
-        gl.glVertex3f( 1.0f,-1.0f, 1.0f);			// Bottom Right Of The Quad (Front)
-
-        gl.glVertex3f( 1.0f,-1.0f,-1.0f);			// Bottom Left Of The Quad (Back)
-        gl.glVertex3f(-1.0f,-1.0f,-1.0f);			// Bottom Right Of The Quad (Back)
-        gl.glVertex3f(-1.0f*x, 1.0f*x,-1.0f*x);			// Top Right Of The Quad (Back)
-        gl.glVertex3f( 1.0f*x, 1.0f*x,-1.0f*x);			// Top Left Of The Quad (Back)
-
-        gl.glVertex3f(-1.0f*x, 1.0f*x, 1.0f*x);			// Top Right Of The Quad (Left)
-        gl.glVertex3f(-1.0f*x, 1.0f*x,-1.0f*x);			// Top Left Of The Quad (Left)
-        gl.glVertex3f(-1.0f,-1.0f,-1.0f);			// Bottom Left Of The Quad (Left)
-        gl.glVertex3f(-1.0f,-1.0f, 1.0f);			// Bottom Right Of The Quad (Left)
-
-        gl.glVertex3f( 1.0f*x, 1.0f*x,-1.0f*x);			// Top Right Of The Quad (Right)
-        gl.glVertex3f( 1.0f*x, 1.0f*x, 1.0f*x);			// Top Left Of The Quad (Right)
-        gl.glVertex3f( 1.0f,-1.0f, 1.0f);			// Bottom Left Of The Quad (Right)
-        gl.glVertex3f( 1.0f,-1.0f,-1.0f);
-        
-        
-        //escada
-        gl.glColor3f(1.0f,1.0f,0f);
-        gl.glVertex3f( (1.0f*x*0.02f)/2, ((1.0f*x)*0.8f), 1.08f*x);			// Top Right Of The Quad (Front)
-        gl.glVertex3f((-1.0f*x*0.02f)/2, ((1.0f*x)*0.8f), 1.08f*x);			// Top Left Of The Quad (Front)
-        gl.glVertex3f((-1.0f*0.02f)/2,-1.0f, 1.0f);			// Bottom Left Of The Quad (Front)
-        gl.glVertex3f( (1.0f*0.02f)/2,-1.0f, 1.0f);	
-        
+            //lateral esquerda
+            gl.glVertex3f( -0.5f, -0.5f, 0.5f);
+            gl.glVertex3f( -0.38f, 0.5f, 0.38f);
+            gl.glVertex3f( -0.38f, 0.5f, -0.38f);
+            gl.glVertex3f( -0.5f, -0.5f, -0.5f);
+            
+            //Latwral direuta
+            gl.glVertex3f( 0.5f, -0.5f, 0.5f);
+            gl.glVertex3f( 0.38f, 0.5f, 0.38f);
+            gl.glVertex3f( 0.38f, 0.5f, -0.38f);
+            gl.glVertex3f( 0.5f, -0.5f, -0.5f);
+            
+        //torre superior
+            //base
+            gl.glVertex3f( -0.38f, 0.5f, 0.38f);
+            gl.glVertex3f( 0.38f, 0.5f, 0.38f);
+            gl.glVertex3f( 0.38f, 0.5f, -0.38f);
+            gl.glVertex3f( -0.38f, 0.5f, -0.38f);
+            
+            //superior
+            gl.glVertex3f( 0.37f, 0.62f, 0.37f);
+            gl.glVertex3f( 0.37f, 0.62f, 0.19f);
+            gl.glVertex3f( 0.37f, 0.5f, 0.19f);
+            gl.glVertex3f( 0.37f, 0.5f, 0.37f);
+                 
+            gl.glVertex3f( 0.37f, 0.62f, -0.37f);
+            gl.glVertex3f( 0.37f, 0.62f, -0.19f);
+            gl.glVertex3f( 0.37f, 0.5f, -0.19f);
+            gl.glVertex3f( 0.37f, 0.5f, -0.37f);
+            
+            gl.glVertex3f( -0.37f, 0.62f, 0.37f);
+            gl.glVertex3f( -0.37f, 0.62f, 0.19f);
+            gl.glVertex3f( -0.37f, 0.5f, 0.19f);
+            gl.glVertex3f( -0.37f, 0.5f, 0.37f);
+            
+            gl.glVertex3f( -0.37f, 0.62f, -0.37f);
+            gl.glVertex3f( -0.37f, 0.62f, -0.19f);
+            gl.glVertex3f( -0.37f, 0.5f, -0.19f);
+            gl.glVertex3f( -0.37f, 0.5f, -0.37f);
+            
+            //espelho2
+            gl.glVertex3f( 0.37f, 0.62f, 0.37f);
+            gl.glVertex3f( 0.19f, 0.62f, 0.37f);
+            gl.glVertex3f( 0.19f, 0.5f, 0.37f);
+            gl.glVertex3f( 0.37f, 0.5f, 0.37f);
+            
+            gl.glVertex3f( 0.37f, 0.62f, -0.37f);
+            gl.glVertex3f( 0.19f, 0.62f, -0.37f);
+            gl.glVertex3f( 0.19f, 0.5f, -0.37f);
+            gl.glVertex3f( 0.37f, 0.5f, -0.37f);
+            
+            gl.glVertex3f( -0.37f, 0.62f, 0.37f);
+            gl.glVertex3f( -0.19f, 0.62f, 0.37f);
+            gl.glVertex3f( -0.19f, 0.5f, 0.37f);
+            gl.glVertex3f( -0.37f, 0.5f, 0.37f);
+            
+            gl.glVertex3f( -0.37f, 0.62f, -0.37f);
+            gl.glVertex3f( -0.19f, 0.62f, -0.37f);
+            gl.glVertex3f( -0.19f, 0.5f, -0.37f);
+            gl.glVertex3f( -0.37f, 0.5f, -0.37f);
+ 
+            //escada
+            gl.glColor3f(1.0f,1.0f,0.0f);
+            gl.glVertex3f( -0.26f, -0.5f, 0.51f);
+            gl.glVertex3f( -0.22f, -0.5f, 0.51f);
+            gl.glVertex3f( -0.22f, 0.26f, 0.42f);
+            gl.glVertex3f( -0.26f, 0.26f, 0.42f);
+            
+            gl.glColor3f(1.0f,1.0f,0.0f);
+            gl.glVertex3f( -0.08f, -0.5f, 0.51f);
+            gl.glVertex3f( -0.04f, -0.5f, 0.51f);
+            gl.glVertex3f( -0.04f, 0.26f, 0.42f);
+            gl.glVertex3f( -0.08f, 0.26f, 0.42f);
+            
+            //degraus
+            
+            gl.glVertex3f( -0.05f, -0.32f, 0.49f);
+            gl.glVertex3f( -0.25f, -0.32f, 0.49f);
+            gl.glVertex3f( -0.25f, -0.38f, 0.50f);
+            gl.glVertex3f( -0.05f, -0.38f, 0.50f);
+            
+            gl.glVertex3f( -0.05f, -0.14f, 0.47f);
+            gl.glVertex3f( -0.25f, -0.14f, 0.47f);
+            gl.glVertex3f( -0.25f, -0.20f, 0.48f);
+            gl.glVertex3f( -0.05f, -0.20f, 0.48f);
+            
+            gl.glVertex3f( -0.05f, 0.02f, 0.45f);
+            gl.glVertex3f( -0.25f, 0.02f, 0.45f);
+            gl.glVertex3f( -0.25f, -0.04f, 0.46f);
+            gl.glVertex3f( -0.05f, -0.04f, 0.46f);
+            
+            gl.glVertex3f( -0.05f, 0.15f, 0.43f);
+            gl.glVertex3f( -0.25f, 0.15f, 0.43f);
+            gl.glVertex3f( -0.25f, 0.21f, 0.42f);
+            gl.glVertex3f( -0.05f, 0.21f, 0.42f);
+            
+            //Janela
+            gl.glColor3f(0.0f,0.0f,0.0f);
+            gl.glVertex3f( 0.06f, 0.28f, 0.41f);
+            gl.glVertex3f( 0.25f, 0.28f, 0.41f);
+            gl.glVertex3f( 0.25f, 0.10f, 0.43f);
+            gl.glVertex3f( 0.06f, 0.10f, 0.43f);
+            
+  
          
         gl.glEnd();
         gl.glPopMatrix();
+        
+        
+        //TORRE 2
+        gl.glPushMatrix();
+        gl.glTranslatef(-2.0f, 0.0f, -6.0f);
+        //gl.glRotatef(rquad, 0f, 1.0f, 0f);
+        gl.glBegin(GL.GL_QUADS);
+        // Torre Base
+            gl.glColor3f(0.83f,0.83f,0.83f);
+            //front
+            gl.glVertex3f( -0.5f, -0.5f, 0.5f);
+            gl.glVertex3f( 0.5f, -0.5f, 0.5f);
+            gl.glVertex3f( 0.38f, 0.5f, 0.38f);
+            gl.glVertex3f( -0.38f, 0.5f, 0.38f);
+            
+            //back
+            gl.glVertex3f( -0.5f, -0.5f, -0.5f);
+            gl.glVertex3f( 0.5f, -0.5f, -0.5f);
+            gl.glVertex3f( 0.38f, 0.5f, -0.38f);
+            gl.glVertex3f( -0.38f, 0.5f, -0.38f);
+
+            //lateral esquerda
+            gl.glVertex3f( -0.5f, -0.5f, 0.5f);
+            gl.glVertex3f( -0.38f, 0.5f, 0.38f);
+            gl.glVertex3f( -0.38f, 0.5f, -0.38f);
+            gl.glVertex3f( -0.5f, -0.5f, -0.5f);
+            
+            //Latwral direuta
+            gl.glVertex3f( 0.5f, -0.5f, 0.5f);
+            gl.glVertex3f( 0.38f, 0.5f, 0.38f);
+            gl.glVertex3f( 0.38f, 0.5f, -0.38f);
+            gl.glVertex3f( 0.5f, -0.5f, -0.5f);
+            
+        //torre superior
+            //base
+            gl.glVertex3f( -0.38f, 0.5f, 0.38f);
+            gl.glVertex3f( 0.38f, 0.5f, 0.38f);
+            gl.glVertex3f( 0.38f, 0.5f, -0.38f);
+            gl.glVertex3f( -0.38f, 0.5f, -0.38f);
+            
+            //superior
+            gl.glVertex3f( 0.37f, 0.62f, 0.37f);
+            gl.glVertex3f( 0.37f, 0.62f, 0.19f);
+            gl.glVertex3f( 0.37f, 0.5f, 0.19f);
+            gl.glVertex3f( 0.37f, 0.5f, 0.37f);
+                 
+            gl.glVertex3f( 0.37f, 0.62f, -0.37f);
+            gl.glVertex3f( 0.37f, 0.62f, -0.19f);
+            gl.glVertex3f( 0.37f, 0.5f, -0.19f);
+            gl.glVertex3f( 0.37f, 0.5f, -0.37f);
+            
+            gl.glVertex3f( -0.37f, 0.62f, 0.37f);
+            gl.glVertex3f( -0.37f, 0.62f, 0.19f);
+            gl.glVertex3f( -0.37f, 0.5f, 0.19f);
+            gl.glVertex3f( -0.37f, 0.5f, 0.37f);
+            
+            gl.glVertex3f( -0.37f, 0.62f, -0.37f);
+            gl.glVertex3f( -0.37f, 0.62f, -0.19f);
+            gl.glVertex3f( -0.37f, 0.5f, -0.19f);
+            gl.glVertex3f( -0.37f, 0.5f, -0.37f);
+            
+            //espelho2
+            gl.glVertex3f( 0.37f, 0.62f, 0.37f);
+            gl.glVertex3f( 0.19f, 0.62f, 0.37f);
+            gl.glVertex3f( 0.19f, 0.5f, 0.37f);
+            gl.glVertex3f( 0.37f, 0.5f, 0.37f);
+            
+            gl.glVertex3f( 0.37f, 0.62f, -0.37f);
+            gl.glVertex3f( 0.19f, 0.62f, -0.37f);
+            gl.glVertex3f( 0.19f, 0.5f, -0.37f);
+            gl.glVertex3f( 0.37f, 0.5f, -0.37f);
+            
+            gl.glVertex3f( -0.37f, 0.62f, 0.37f);
+            gl.glVertex3f( -0.19f, 0.62f, 0.37f);
+            gl.glVertex3f( -0.19f, 0.5f, 0.37f);
+            gl.glVertex3f( -0.37f, 0.5f, 0.37f);
+            
+            gl.glVertex3f( -0.37f, 0.62f, -0.37f);
+            gl.glVertex3f( -0.19f, 0.62f, -0.37f);
+            gl.glVertex3f( -0.19f, 0.5f, -0.37f);
+            gl.glVertex3f( -0.37f, 0.5f, -0.37f);
+ 
+            //escada
+            gl.glColor3f(1.0f,1.0f,0.0f);
+            gl.glVertex3f( -0.26f, -0.5f, 0.51f);
+            gl.glVertex3f( -0.22f, -0.5f, 0.51f);
+            gl.glVertex3f( -0.22f, 0.26f, 0.42f);
+            gl.glVertex3f( -0.26f, 0.26f, 0.42f);
+            
+            gl.glColor3f(1.0f,1.0f,0.0f);
+            gl.glVertex3f( -0.08f, -0.5f, 0.51f);
+            gl.glVertex3f( -0.04f, -0.5f, 0.51f);
+            gl.glVertex3f( -0.04f, 0.26f, 0.42f);
+            gl.glVertex3f( -0.08f, 0.26f, 0.42f);
+            
+            //degraus
+            
+            gl.glVertex3f( -0.05f, -0.32f, 0.49f);
+            gl.glVertex3f( -0.25f, -0.32f, 0.49f);
+            gl.glVertex3f( -0.25f, -0.38f, 0.50f);
+            gl.glVertex3f( -0.05f, -0.38f, 0.50f);
+            
+            gl.glVertex3f( -0.05f, -0.14f, 0.47f);
+            gl.glVertex3f( -0.25f, -0.14f, 0.47f);
+            gl.glVertex3f( -0.25f, -0.20f, 0.48f);
+            gl.glVertex3f( -0.05f, -0.20f, 0.48f);
+            
+            gl.glVertex3f( -0.05f, 0.02f, 0.45f);
+            gl.glVertex3f( -0.25f, 0.02f, 0.45f);
+            gl.glVertex3f( -0.25f, -0.04f, 0.46f);
+            gl.glVertex3f( -0.05f, -0.04f, 0.46f);
+            
+            gl.glVertex3f( -0.05f, 0.15f, 0.43f);
+            gl.glVertex3f( -0.25f, 0.15f, 0.43f);
+            gl.glVertex3f( -0.25f, 0.21f, 0.42f);
+            gl.glVertex3f( -0.05f, 0.21f, 0.42f);
+            
+            //Janela
+            gl.glColor3f(0.0f,0.0f,0.0f);
+            gl.glVertex3f( 0.06f, 0.28f, 0.41f);
+            gl.glVertex3f( 0.25f, 0.28f, 0.41f);
+            gl.glVertex3f( 0.25f, 0.10f, 0.43f);
+            gl.glVertex3f( 0.06f, 0.10f, 0.43f);
+            
+  
+         
+        gl.glEnd();
+        gl.glPopMatrix();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // Arena
+            gl.glPushMatrix();
+            gl.glTranslatef(1.9f, 0.0f, -6.0f);
+            //gl.glRotatef(rquad, 0f, 1.0f, 0f);
+            gl.glBegin(GL.GL_QUADS);
+            gl.glColor3f(0.48f,0.63f,0.14f);
+            gl.glVertex3f( 0.84f, -0.5f, 1.56f);
+            gl.glVertex3f( -6f, -0.5f, 1.56f);
+            gl.glVertex3f( -6f, -0.5f, -12.56f);
+            gl.glVertex3f( 0.84f, -0.5f, -12.56f);
+            gl.glEnd();
+            gl.glPopMatrix();
+            
+            
+            
+            
         gl.glFlush();
       
       //rotação do quadrado
-      rquad += 0.1f;
+     // rquad += 0.5f;
     }
     
     
@@ -112,7 +360,7 @@ public class TorreArk
         gl.glViewport(0, 0, width, height);
         gl.glMatrixMode(GL.GL_PROJECTION);
         gl.glLoadIdentity();
-        glu.gluPerspective(90.0f, h, 1.0, 20.0);
+        glu.gluPerspective(45.0f, h, 5.0, 50.0);
         gl.glMatrixMode(GL.GL_MODELVIEW);
         gl.glLoadIdentity();
         
